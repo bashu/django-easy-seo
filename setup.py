b6c2250c3668478717da2bcb2870b5c497226fb6
Basil Shubin <basil.shubin@gmail.com>
2013-09-26 10:09:12 +0700

    change package name

--- setup.py
+++ setup.py
@@ -7,11 +7,11 @@ README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name="django-seo",
+    name="django-easy-seo",
     version='0.3.7',
     packages=['seo'],
     include_package_data=True,
-    license="GPLv3",
+    license="GPLv3 License",
     description = "Adds generic SEO fields for objects in your site or specific urls",
     long_description=README,
     author="Alexander Ivanov",
