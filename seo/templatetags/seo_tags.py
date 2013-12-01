f5d2db80fd6b81e23c6a1767ae476985c1629891
Basil Shubin <basil.shubin@gmail.com>
2013-12-01 13:33:33 +0700

    update cache keys

--- seo/templatetags/seo_tags.py
+++ seo/templatetags/seo_tags.py
@@ -40,14 +40,14 @@ class SeoNode(template.Node):
         site = get_current_site(request)
         target_object = self.get_target_object(context)
         if target_object is None:
-            cache_key = 'SEO:%s' % make_key('%s.path%s:%s' % (site.pk, request.path_info, self.intent))
+            cache_key = 'SEO:%s' % make_key('%s.%s:%s' % (site.pk, request.path_info, self.intent))
             value = cache.get(cache_key)
             if value is None:
                 seobj = Url.objects.get_seo_object(request.path_info, site)
                 value = getattr(seobj, self.intent, None)
                 cache.set(cache_key, value, CACHE_TIMEOUT)  # store in a cache
         else:
-            cache_key = 'SEO:%s' % make_key('%s.pk%s:%s' % (site.pk, target_object.pk, self.intent))
+            cache_key = 'SEO:%s' % make_key('%s.%s:%s' % (site.pk, target_object.pk, self.intent))
             value = cache.get(cache_key)
             if value is None:
                 seobj = Seo.objects.get_seo_object(target_object, site)
