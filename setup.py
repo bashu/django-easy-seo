eaad4ca90713036788324e604f28704545954a54
Basil Shubin <basil.shubin@gmail.com>
2013-12-02 11:00:01 +0700

    bump version number

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name="django-easy-seo",
-    version='0.3.8.2',
+    version='0.3.8.3',
     packages=['seo'],
     include_package_data=True,
     license="GPLv3 License",
