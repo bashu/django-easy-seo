c1d432648efb2f4614f5559caa69f187c45907d7
Basil Shubin <basil.shubin@gmail.com>
2014-01-22 13:43:23 +0700

    update tag library

--- seo/templatetags/seo_tags.py
+++ seo/templatetags/seo_tags.py
@@ -3,12 +3,15 @@
 from django import template
 from django.core.cache import cache
 from django.utils.html import escape
+from django.db.models.base import ModelBase
+from django.core.handlers.wsgi import WSGIRequest
+from django.contrib.sites.models import get_current_site
 
 from classytags.core import Tag, Options
 from classytags.arguments import Argument, ChoiceArgument
 
 from ..settings import INTENTS, CACHE_PREFIX, CACHE_TIMEOUT
-from ..models import Seo, make_key
+from ..models import Seo, URL, make_key
 
 register = template.Library()
 
@@ -24,14 +27,30 @@ class SeoTag(Tag):
     )
 
     def render_tag(self, context, intent, instance, varname):
-        cache_key = '%s:%s' % (CACHE_PREFIX, make_key(
-            instance, instance.pk, intent))
+        if isinstance(instance, ModelBase):
+            cache_key = '%s:%s' % (CACHE_PREFIX, make_key(
+                instance, instance.pk, intent))
 
-        value = cache.get(cache_key)
-        if not value:
-            seobj = Seo.objects.get_seo_object(instance)
-            value = getattr(seobj, intent, None)
-            cache.set(cache_key, value, CACHE_TIMEOUT)  # store in a cache
+            value = cache.get(cache_key)
+            if not value:
+                seobj = Seo.objects.get_seo_object(instance)
+                value = getattr(seobj, intent, None)
+                cache.set(cache_key, value, CACHE_TIMEOUT)  # store in a cache
+
+        elif isinstance(instance, WSGIRequest):
+            request = instance
+            current_site = get_current_site(request)
+            cache_key = '%s:%s%s:%s' % (
+                CACHE_PREFIX, current_site.domain, request.path, intent)
+
+            value = cache.get(cache_key)
+            if not value:
+                seobj = URL.objects.get_seo_object(request.path, current_site)
+                value = getattr(seobj, intent, None)
+                cache.set(cache_key, value, CACHE_TIMEOUT)  # store in a cache
+
+        elif isinstance(instance, basestring):
+            raise NotImplementedError
 
         if varname:
             context[varname] = value
